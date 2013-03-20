206079bc01c52be46c95b3df20ca4d325b92cdc7
java.luozheng@gmail.com <java.luozheng@gmail.com@1fc3ba73-0b2d-9c06-0eea-3f0574e054fe>
2013-03-20 13:00:11 +0000

    这是罗拯写的一个for循环的类

---
+++ platfrom/src/com/test/LzTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class LzTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int x=0;x<5;x++){
+			System.out.println(x);
+		}
+	}
+
+}
