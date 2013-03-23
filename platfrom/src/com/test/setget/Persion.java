5e1d527c40251e6671d8f51d561624cfaf323ab5
java.luozheng@gmail.com <java.luozheng@gmail.com@1fc3ba73-0b2d-9c06-0eea-3f0574e054fe>
2013-03-23 05:22:06 +0000

    对象的封装  set get方法   一个带参数   一个带返回值

---
+++ platfrom/src/com/test/setget/Persion.java
@@ -0,0 +1,22 @@
+package com.test.setget;
+
+public class Persion {		//对象的封装
+
+	private int x;
+	private int y;
+	
+	public int getX() {		//得到一个值
+		return x;
+	}
+	public void setX(int x) {//设置一个值
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
---
+++ platfrom/src/com/test/setget/SetGetTest.java
@@ -0,0 +1,15 @@
+package com.test.setget;
+
+public class SetGetTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Persion p=new Persion();
+		p.setX(10);
+		System.out.println(p.getX());
+
+	}
+
+}
